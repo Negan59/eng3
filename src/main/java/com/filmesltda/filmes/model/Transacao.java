e547c64fe81ffb7ca5e4a32890a69062444ca409
Your Name <you@example.com>
2022-09-19 11:32:40 -0300

    atualização

--- src/main/java/com/filmesltda/filmes/model/Transacao.java
+++ src/main/java/com/filmesltda/filmes/model/Transacao.java
@@ -1,5 +1,63 @@
 package com.filmesltda.filmes.model;
 
-public class Transacao {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public abstract class Transacao {
+    private int id;
+    private LocalDateTime data;
+    private Produto produto;
+    private Usuario usuario;
+    public Transacao() {
+    }
+    public Transacao(LocalDateTime data, Produto produto, Usuario usuario) {
+        this.data = data;
+        this.produto = produto;
+        this.usuario = usuario;
+    }
+    public Transacao(int id, LocalDateTime data, Produto produto, Usuario usuario) {
+        this.id = id;
+        this.data = data;
+        this.produto = produto;
+        this.usuario = usuario;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public LocalDateTime getData() {
+        return data;
+    }
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+    public Produto getProduto() {
+        return produto;
+    }
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+    public Usuario getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public abstract boolean salvar();
+    
+    public boolean excluir(int id){
+        return true;
+    }
+
+    /*public Transacao buscar_um(int id){
+        
+    }*/
+
+    /*public ArrayList<Transacao> buscarTodos(){
+
+    }*/
     
 }
