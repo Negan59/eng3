aae1b1b759438571b133038ef070423fc941fc46
Your Name <you@example.com>
2022-09-19 11:20:47 -0300

    fix local

--- src/main/java/com/eleicoes/eleicoes/repository/VotoDAO.java
+++
@@ -1,9 +0,0 @@
-package com.eleicoes.eleicoes.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.eleicoes.eleicoes.model.Voto;
-
-public interface VotoDAO extends JpaRepository<Voto, Long> {
-
-}
